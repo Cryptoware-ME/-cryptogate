bebe7240d634a9a54197ce4cff5f66f400a670a5
KarimDeaibes <deaibes.karim@outlook.com>
2022-04-27 10:20:50 +0300

    Removed all references to non-cryptogate packages

--- packages/react-ui/src/config/dapp.tsx
+++ packages/react-ui/src/config/dapp.tsx
@@ -1,32 +1,34 @@
-import { getChainById, ChainId } from '@usedapp/core';
+import { useDapp } from "@cryptogate/react-providers";
+
+const { ChainId, getChainById } = useDapp;
 
 const DappConfig = {
-    readOnlyUrls: {
-      [ChainId.Mainnet]: 'https://eth-node.oasisx.world/',
-      [ChainId.Rinkeby]: 'https://rink-node.oasisx.world/',
-      [ChainId.Mumbai]: 'https://rpc.maticvigil.com/',
-      [ChainId.Polygon]: 'https://polygon-node.oasisx.world/',
-      [ChainId.BSC]: 'https://bsc-node.oasisx.world/',
-      [ChainId.Avalanche]: 'https://avalanche-node.oasisx.world/',
-    },
-    appName: 'Cryptogate',
-    appEmail: 'cryptoware@yahoo.com',
-    appUrl: 'cryptoware.me',
-    appLogo: '',
-    pollingInterval: 1000,
-    networks: [
-      getChainById(ChainId.Rinkeby), 
-      getChainById(ChainId.Mainnet), 
-      getChainById(ChainId.Mumbai),
-      getChainById(ChainId.Polygon),
-      getChainById(ChainId.BSC),
-      getChainById(ChainId.Avalanche)
-    ],
-    notifications: {
-      checkInterval: 1000,
-      expirationPeriod: 10000,
-    },
-    autoConnect: false,
-}
+  readOnlyUrls: {
+    [ChainId.Mainnet]: "https://eth-node.oasisx.world/",
+    [ChainId.Rinkeby]: "https://rink-node.oasisx.world/",
+    [ChainId.Mumbai]: "https://rpc.maticvigil.com/",
+    [ChainId.Polygon]: "https://polygon-node.oasisx.world/",
+    [ChainId.BSC]: "https://bsc-node.oasisx.world/",
+    [ChainId.Avalanche]: "https://avalanche-node.oasisx.world/",
+  },
+  appName: "Cryptogate",
+  appEmail: "cryptoware@yahoo.com",
+  appUrl: "cryptoware.me",
+  appLogo: "",
+  pollingInterval: 1000,
+  networks: [
+    getChainById(ChainId.Rinkeby),
+    getChainById(ChainId.Mainnet),
+    getChainById(ChainId.Mumbai),
+    getChainById(ChainId.Polygon),
+    getChainById(ChainId.BSC),
+    getChainById(ChainId.Avalanche),
+  ],
+  notifications: {
+    checkInterval: 1000,
+    expirationPeriod: 10000,
+  },
+  autoConnect: false,
+};
 
 export default DappConfig;
