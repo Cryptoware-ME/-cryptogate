6ad3a1d237a7010df9651e3e56d41f141f95e173
KarimDeaibes <deaibes.karim@outlook.com>
2022-04-27 11:07:16 +0300

    Removed Config folder from react-ui

--- packages/react-ui/dist/cjs/dist/config/solapp.d.ts
+++
@@ -1,8 +0,0 @@
-declare const SolappConfig: {
-    config: {
-        env: string;
-        autoConnect: boolean;
-        lamportsPerSol: number;
-    };
-};
-export default SolappConfig;
--- packages/react-ui/src/config/index.tsx
+++
@@ -1,8 +0,0 @@
-import DappConfig from "./dapp";
-import SolappConfig from "./solapp";
-
-const config = {
-  DappConfig: { ...DappConfig },
-  SolConfig: SolappConfig,
-};
-export default config;
